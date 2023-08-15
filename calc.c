32c8fa320fbe54a21ebe3c0a42e00a82d4afeb1c
Cimorexave <sadeq.s502@gmail.com>
2023-08-15 09:43:56 +0330

    testing string arith.

--- calc.c
+++ calc.c
@@ -5,11 +5,18 @@
 
 int main(int argc, char **argv)
 {
-    printf("write down your equation: (+-/*)");
+    // printf("write down your equation: (+-/*)");
 
-    char *input;
+    // char *input;
 
-    input = scanf("%s");
+    // scanf("%s", &input);
+
+    // printf("this is your equation: \t %s", input);
+
+    char *my_str = "Hello there!";
+    printf("my_str: %s\n", my_str);
+    printf("address of my_str(pointer to my_str): %p\n", &my_str);
+    printf("first index of my_str(my_str[0]): %c\n", my_str[0]);
 
     return 0;
 }
