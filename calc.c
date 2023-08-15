512334769f4d8f776e9bc86a819cb14c11ea1a8c
Cimorexave <sadeq.s502@gmail.com>
2023-08-15 10:04:35 +0330

    accepting string input

--- calc.c
+++ calc.c
@@ -5,18 +5,25 @@
 
 int main(int argc, char **argv)
 {
-    // printf("write down your equation: (+-/*)");
+    char *input = malloc(100 * sizeof(char));
+    // char **p_input = &input;
+    printf("write down your equation: (+-/*)\n");
 
-    // char *input;
-
-    // scanf("%s", &input);
-
+    scanf("%[^\n]s", input);
     // printf("this is your equation: \t %s", input);
 
-    char *my_str = "Hello there!";
-    printf("my_str: %s\n", my_str);
-    printf("address of my_str(pointer to my_str): %p\n", &my_str);
-    printf("first index of my_str(my_str[0]): %c\n", my_str[0]);
+    for (int i = 0; i < strlen(input); i++)
+    {
+    }
+
+    free(input);
+    // char *my_str = "Hello there!";
+    // printf("my_str: %s\n", my_str);
+    // printf("address of my_str(pointer to my_str): %p\n", &my_str);
+    // printf("first index of my_str(my_str[0]): %c\n", my_str[0]);
+    // char **pointer_to_my_str = &my_str;
+    // printf("value of p: %s\n", *pointer_to_my_str);
+    // printf("p: %p\n", pointer_to_my_str);
 
     return 0;
 }
