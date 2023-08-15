39a92de18de08076dd7914c7f51cd8e2c5941a0b
Cimorexave <sadeq.s502@gmail.com>
2023-08-15 09:32:18 +0330

    calculator init

---
+++ calc.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+
+int main(int argc, char **argv)
+{
+    printf("write down your equation: (+-/*)");
+
+    char *input;
+
+    input = scanf("%s");
+
+    return 0;
+}
